e3ec2c17e1e345e8de72429f459b11df0d36b237
Alexandre WEISSER <alexandre.weisser@gmail.com>
2020-11-22 22:50:37 +0100

    feat : footer with info

--- Components/Footer/index.tsx
+++ Components/Footer/index.tsx
@@ -5,11 +5,17 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
     color : gray;
     font-size : 0.8em;
+
+    display: flex;
+    flex-direction: column;
 `;
 
 const Footer = ()=>{
     return <Wrapper>
         <p>Designed by Alessia Sanna, Delivered by Alexandre Weisser</p>
+        <a href = "mailto: a.sannaprojects@gmail.com">a.sannaprojects@gmail.com</a>
+
+        <a href="tel:06.74.00.25.70">06.74.00.25.70</a>
     </Wrapper>
 }
 
