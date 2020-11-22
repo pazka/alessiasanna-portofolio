6b791c1fc64ec4ccca3a7a80bb5bd836e99764b6
Alexandre WEISSER <alexandre.weisser@gmail.com>
2020-11-22 15:16:51 +0100

    Errors for missing ressources

--- Resources/Images/index.tsx
+++ Resources/Images/index.tsx
@@ -1,8 +1,18 @@
-const images = require('../../src/images/**/*.jpg')
+ 
+try{
+    const images = require('../../src/images/**/*.jpg')
+}catch(err){
+    console.error( "No images loaded")
+    const images = {}
+}
 
 
 class Images {
     constructor(){
+        if(images.length == 0){
+            console.error("No Images loaded")
+        }
+
         Object.keys(images).forEach(key => {
             this[key] = images[key]
         });
--- Resources/Texts/index.tsx
+++ Resources/Texts/index.tsx
@@ -1,24 +1,38 @@
-const texts = require('../../src/texts/**/*.txt')
+
+try{
+    const texts = require('../../src/texts/**/*.txt')
+}catch(err){
+    console.error( "No texts loaded")
+    const texts = {}
+}
+
 
 class Texts {
     AvailableFiles = {};
 
     constructor(){
+        if(texts.length == 0){
+            console.error("No texts loaded")
+        }
+
         Object.keys(texts).forEach(key => {
             this.AvailableFiles[key] = texts[key].desc
         });        
     }
 
     GetText(filePath):String{
-        
-        var result = null;
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.open("GET", filePath, false);
-        xmlhttp.send();
-        if (xmlhttp.status==200) {
-            result = xmlhttp.responseText;
+        try{
+            var result = null;
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.open("GET", filePath, false);
+            xmlhttp.send();
+            if (xmlhttp.status==200) {
+                result = xmlhttp.responseText;
+            }
+            return result;
+        }catch(err){
+            return "No text available."
         }
-        return result;
     }
     //could add post-treatments here
 }
